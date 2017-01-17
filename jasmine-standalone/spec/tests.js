076109d155eb47af57283745b1a434bb0bab63e3
Jayson Buquia <buquia.jace@gmail.com>
2017-01-17 15:30:03 +0800

    Class and extensions support tests

--- jasmine-standalone/spec/tests.js
+++ jasmine-standalone/spec/tests.js
@@ -24,6 +24,16 @@ describe('Main Application', function () {
 		expect(App.get('InitialModule').name).toBe('InitialModule');
 	});
 
+	it('should support ES2015 classes as constructors', function () {
+		var ModuleClass1 = class {
+			constructor ( name ) {
+				this.moduleName = 'Module Class 1'
+			}
+		}
+		App.module('ModuleClass1',ModuleClass1);
+		expect(App.get('ModuleClass1') instanceof ModuleClass1 ).toBe(true);
+	})
+
 	it('does not allow a module to be redefined', function () {
 		var FourthModule = function(){
 		};
@@ -44,7 +54,7 @@ describe('Main Application', function () {
 
 	describe('Dependency injection', function () {
 
-		it('able to preload dependencies before loading a module.', function () {
+		it('preloads dependencies before loading a module.', function () {
 			var FirstModule = function(SecondModule){
 				this.secondModuleName = SecondModule.name
 			};
@@ -61,6 +71,39 @@ describe('Main Application', function () {
 			expect(App.get('FirstModule').secondModuleName).toBe(SecondModule.prototype.name);
 			expect(App.get('ThirdModule').firstModuleName).toBe(FirstModule.prototype.name);
 
+		});
+
+		it('allows extension of constructors/classes', function () {
+			class BaseClass {
+				getModuleName () {
+					return this.moduleName;
+				}
+			}
+			class SubClass extends BaseClass {
+				constructor () {
+					super();
+					this.moduleName = 'Sub Class';
+				}
+			}
+			var BaseConstructor = function () {
+				this.getModuleName = function () {
+					return this.moduleName;
+				}
+			}
+			class BaseConstructorChild extends BaseConstructor {
+				constructor () {
+					super();
+					this.moduleName = 'BaseConstructorChild Class';
+				}
+			}
+
+			App.module('BaseClass',BaseClass);
+			App.module('SubClass',['BaseClass'],SubClass);
+			App.module('BaseConstructor',BaseConstructor);
+			App.module('BaseConstructorChild',['BaseConstructor'],BaseConstructorChild);
+
+			expect(App.get('SubClass').getModuleName()).toEqual('Sub Class');
+			expect(App.get('BaseConstructorChild').getModuleName()).toEqual('BaseConstructorChild Class');
 		});
 
 	});
